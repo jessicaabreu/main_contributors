8e9b99aa4b73fad1e3aac437490915b7e36d5b23
jessicaabreu <jxd484@case.edu>
2019-09-05 10:57:30 -0400

    BUGFIX: groups were not properly split when a certain number of students were missing

--- create_fake_scores.m
+++ create_fake_scores.m
@@ -30,7 +30,7 @@ student_ids = 1:number_of_students;
 max_missing = ceil(number_of_students * 0.2);
 % The first ids will be the players
 players = 1:number_of_players;
-score_file.players = players;
+% score_file.players = players;
 
 for l=1:number_of_lectures
     lecture_name = strcat('lecture', num2str(l));
@@ -45,29 +45,34 @@ for l=1:number_of_lectures
     idx1 = randperm(length(class_final)); % first round
     idx2 = randperm(length(class_final)); % second round
     std_per_group = floor(length(class_final)/number_of_groups);
+    rest = mod(length(class_final), number_of_groups);
     for g=1:number_of_groups
         % Takes care of different number of students per group
-        if g == number_of_groups
-            students_r1 = class_final(idx1(1+(g-1)*std_per_group:end));
-            students_r2 = class_final(idx2(1+(g-1)*std_per_group:end));
-        else
+        if g <= number_of_groups - rest
             students_r1 = class_final(idx1(1+(g-1)*std_per_group:g*std_per_group));
             students_r2 = class_final(idx2(1+(g-1)*std_per_group:g*std_per_group));
+        else
+            students_r1 = class_final(idx1(1+(g-1)*std_per_group:g*std_per_group+1));
+            students_r2 = class_final(idx2(1+(g-1)*std_per_group:g*std_per_group+1));
         end
         score_file.(lecture_name).('r1').(strcat('group', num2str(g))). ...
             students = students_r1;
+        score_file.(lecture_name).('r2').(strcat('group', num2str(g))). ...
+            students = students_r2;
         % For the first round, grades will be randomly selected from normal
         % distributions centered in 5, with std 1.5
         % If a player is in the team, the score is higher
         idx_player_1 = ismember(students_r1, players);
-        grades_1 = normrnd(5 + increase_per_player * nnz(idx_player_1) * 5, 1.5,[1,length(students_r1)]);
+        grades_1 = normrnd(5 + increase_per_player * nnz(idx_player_1) * 5, ...
+            1.5,[1,length(students_r1)]);
         score_file.(lecture_name).('r1').(strcat('group', num2str(g))). ...
             grades = grades_1;
         % For the second round, grades will be randomly selected from normal
         % distributions centered in 7, with std 1.0
         % If a player is in the team, the score is higher
         idx_player_2 = ismember(students_r2, players);
-        grades_2 = normrnd(5 + increase_per_player * nnz(idx_player_2) * 5, 1.5,[1,length(students_r2)]);
+        grades_2 = normrnd(5 + increase_per_player * nnz(idx_player_2) * 5, ...
+            1.5,[1,length(students_r2)]);
         score_file.(lecture_name).('r2').(strcat('group', num2str(g))). ...
             grades = grades_2;
     end
